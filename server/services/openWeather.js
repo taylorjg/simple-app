5e1fc7005e2635983a1b1265056d9403a96bd0a8
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-01-02 20:19:30 +0000

    Use axios config.params instead of query string

--- server/services/openWeather.js
+++ server/services/openWeather.js
@@ -4,12 +4,18 @@ const configure = apiKey => {
 
   const axiosInstance = axios.create({
     baseURL: 'https://api.openweathermap.org'
-    // TODO: can we add a header here rather than include APPID={apiKey} in every URL ?
   })
 
   const getWeatherInfo = async (country, city) => {
-    const url = `/data/2.5/weather?q=${city},${country}&APPID=${apiKey}`
-    const response = await axiosInstance.get(url)
+    const url = `/data/2.5/weather`
+    const config = {
+      params: {
+        APPID: apiKey,
+        q: `${city},${country}`,
+        units: 'metric'
+      }
+    }
+    const response = await axiosInstance.get(url, config)
     return response.data
   }
 
