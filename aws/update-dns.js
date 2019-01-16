06c75451eb123652ac33cb987d9a324750e58db8
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-01-16 20:59:09 +0000

    Refactor update_dns tool

--- aws/update-dns.js
+++ aws/update-dns.js
@@ -3,81 +3,102 @@ const R = require('ramda')
 
 const CLUSTER_NAME = 'simple-app'
 
+const findTask = async services => {
+  const params = {
+    cluster: CLUSTER_NAME
+  }
+  const response = await services.ecs.listTasks(params).promise()
+  const taskArn = response.taskArns[0]
+  console.log(`taskArn: ${taskArn}`)
+  return taskArn
+}
+
+const findEni = async (services, taskArn) => {
+  const params = {
+    cluster: CLUSTER_NAME,
+    tasks: [taskArn]
+  }
+  const response = await services.ecs.describeTasks(params).promise()
+  const attachmentsDetails = response.tasks[0].attachments[0].details
+  const eni = attachmentsDetails.find(d => d.name === 'networkInterfaceId').value
+  console.log(`eni: ${eni}`)
+  return eni
+}
+
+const findNewPublicIp = async (services, eni) => {
+  const params = {
+    NetworkInterfaceIds: [eni]
+  }
+  const response = await services.ec2.describeNetworkInterfaces(params).promise()
+  const newPublicIp = response.NetworkInterfaces[0].Association.PublicIp
+  console.log(`newPublicIp: ${newPublicIp}`)
+  return newPublicIp
+}
+
+const findHostedZoneId = async (services, dnsName) => {
+  const params = {
+    DNSName: dnsName
+  }
+  const response = await services.route53.listHostedZonesByName(params).promise()
+  const hostedZoneId = response.HostedZones[0].Id
+  console.log(`hostedZoneId: ${hostedZoneId}`)
+  return hostedZoneId
+}
+
+const findDnsRecordSet = async (services, hostedZoneId) => {
+  const params = {
+    HostedZoneId: hostedZoneId
+  }
+  const response = await services.route53.listResourceRecordSets(params).promise()
+  const dnsRecordSet = response.ResourceRecordSets.find(r => r.Type === 'A')
+  console.log(`dnsRecordSet: ${JSON.stringify(dnsRecordSet, null, 2)}`)
+  return dnsRecordSet
+}
+
+const updateDnsRecordSet = async (services, hostedZoneId, dnsRecordSet, newPublicIp) => {
+
+  const dnsRecordSetClone = R.clone(dnsRecordSet)
+  dnsRecordSetClone.ResourceRecords[0].Value = newPublicIp
+  console.log(`resourceRecordSetClone: ${JSON.stringify(dnsRecordSetClone, null, 2)}`)
+
+  const changeParams = {
+    HostedZoneId: hostedZoneId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'UPSERT',
+          ResourceRecordSet: dnsRecordSetClone
+        }
+      ]
+    }
+  }
+  const changeResponse = await services.route53.changeResourceRecordSets(changeParams).promise()
+  console.dir(changeResponse)
+
+  const waitParams = {
+    Id: changeResponse.ChangeInfo.Id
+  }
+  const waitResponse = await services.route53.waitFor('resourceRecordSetsChanged', waitParams).promise()
+  console.dir(waitResponse)
+}
+
 const main = async () => {
   try {
-    const ecs = new AWS.ECS({ apiVersion: '2014-11-13' })
-    const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
-    const route53 = new AWS.Route53({ apiVersion: '2013-04-01' })
-
-    const listTasksParams = {
-      cluster: CLUSTER_NAME
+    const services = {
+      ecs: new AWS.ECS({ apiVersion: '2014-11-13' }),
+      ec2: new AWS.EC2({ apiVersion: '2016-11-15' }),
+      route53: new AWS.Route53({ apiVersion: '2013-04-01' })
     }
-    const listTasksResponse = await ecs.listTasks(listTasksParams).promise()
 
-    const taskArn = listTasksResponse.taskArns[0]
-    console.log(`taskArn: ${taskArn}`)
-
-    const describeTasksParams = {
-      cluster: CLUSTER_NAME,
-      tasks: [taskArn]
-    }
-    const describeTasksResponse = await ecs.describeTasks(describeTasksParams).promise()
-
-    const attachmentsDetails = describeTasksResponse.tasks[0].attachments[0].details
-    const eni = attachmentsDetails.find(d => d.name === 'networkInterfaceId').value
-    console.log(`eni: ${eni}`)
-
-    const describeNetworkInterfacesParams = {
-      NetworkInterfaceIds: [eni]
-    }
-    const describeNetworkInterfacesResponse = await ec2.describeNetworkInterfaces(describeNetworkInterfacesParams).promise()
-    const newPublicIp = describeNetworkInterfacesResponse.NetworkInterfaces[0].Association.PublicIp
-    console.log(`newPublicIp: ${newPublicIp}`)
-
-    const listHostedZonesByNameParams = {
-      DNSName: 'simple-app-jt.com.'
-    }
-    const listHostedZonesByNameResponse = await route53.listHostedZonesByName(listHostedZonesByNameParams).promise()
-
-    const hostedZoneId = listHostedZonesByNameResponse.HostedZones[0].Id
-    console.log(`hostedZoneId: ${hostedZoneId}`)
-
-    const listResourceRecordSetsParams = {
-      HostedZoneId: hostedZoneId
-    }
-    const listResourceRecordSetsResponse = await route53.listResourceRecordSets(listResourceRecordSetsParams).promise()
-
-    const resourceRecordSet = listResourceRecordSetsResponse.ResourceRecordSets.find(r => r.Type === 'A')
-    console.log(`resourceRecordSet: ${JSON.stringify(resourceRecordSet, null, 2)}`)
-
-    const oldPublicIp = resourceRecordSet.ResourceRecords[0].Value
+    const taskArn = await findTask(services)
+    const eni = await findEni(services, taskArn)
+    const newPublicIp = await findNewPublicIp(services, eni)
+    const hostedZoneId = await findHostedZoneId(services, 'simple-app-jt.com')
+    const dnsRecordSet = await findDnsRecordSet(services, hostedZoneId)
+    const oldPublicIp = dnsRecordSet.ResourceRecords[0].Value
     console.log(`oldPublicIp: ${oldPublicIp}`)
-
     if (newPublicIp !== oldPublicIp) {
-
-      const resourceRecordSetClone = R.clone(resourceRecordSet)
-      resourceRecordSetClone.ResourceRecords[0].Value = newPublicIp
-      console.log(`resourceRecordSetClone: ${JSON.stringify(resourceRecordSetClone, null, 2)}`)
-
-      const changeResourceRecordSetsParams = {
-        HostedZoneId: hostedZoneId,
-        ChangeBatch: {
-          Changes: [
-            {
-              Action: 'UPSERT',
-              ResourceRecordSet: resourceRecordSetClone
-            }
-          ]
-        }
-      }
-      const changeResourceRecordSetsResponse = await route53.changeResourceRecordSets(changeResourceRecordSetsParams).promise()
-      console.dir(changeResourceRecordSetsResponse)
-
-      const waitParams = {
-        Id: changeResourceRecordSetsResponse.ChangeInfo.Id
-      }
-      const waitResponse = await route53.waitFor('resourceRecordSetsChanged', waitParams).promise()
-      console.dir(waitResponse)
+      await updateDnsRecordSet(services, hostedZoneId, dnsRecordSet, newPublicIp)
     }
   } catch (error) {
     console.log(`[main] error: ${error.message}`)
