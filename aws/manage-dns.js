9e3b8002ccb029be8fec232b33d64e47747248d7
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-02-01 20:37:22 +0000

    More tweaks to manage-dns.js

--- aws/manage-dns.js
+++ aws/manage-dns.js
@@ -2,6 +2,12 @@ const AWS = require('aws-sdk')
 const R = require('ramda')
 
 const CLUSTER_NAME = 'simple-app'
+const DOMAIN_NAME = 'simple-app-jt.com.'
+const TTL = 60
+
+const toJson = v => JSON.stringify(v, null, 2)
+
+const show = (value, xf = R.identity) => value ? xf(value) : '(none)'
 
 const findTask = async services => {
   const params = {
@@ -9,7 +15,7 @@ const findTask = async services => {
   }
   const response = await services.ecs.listTasks(params).promise()
   const taskArn = response.taskArns[0]
-  console.log(`taskArn: ${taskArn}`)
+  console.log(`[findTask] taskArn: ${show(taskArn)}`)
   return taskArn
 }
 
@@ -21,7 +27,7 @@ const findEni = async (services, taskArn) => {
   const response = await services.ecs.describeTasks(params).promise()
   const attachmentsDetails = response.tasks[0].attachments[0].details
   const eni = attachmentsDetails.find(d => d.name === 'networkInterfaceId').value
-  console.log(`eni: ${eni}`)
+  console.log(`[findEni] eni: ${show(eni)}`)
   return eni
 }
 
@@ -31,7 +37,7 @@ const findNewPublicIp = async (services, eni) => {
   }
   const response = await services.ec2.describeNetworkInterfaces(params).promise()
   const newPublicIp = response.NetworkInterfaces[0].Association.PublicIp
-  console.log(`newPublicIp: ${newPublicIp}`)
+  console.log(`[findNewPublicIp] newPublicIp: ${show(newPublicIp)}`)
   return newPublicIp
 }
 
@@ -41,7 +47,7 @@ const findHostedZoneId = async (services, dnsName) => {
   }
   const response = await services.route53.listHostedZonesByName(params).promise()
   const hostedZoneId = response.HostedZones[0].Id
-  console.log(`hostedZoneId: ${hostedZoneId}`)
+  console.log(`[findHostedZoneId] hostedZoneId: ${show(hostedZoneId)}`)
   return hostedZoneId
 }
 
@@ -51,122 +57,70 @@ const findDnsRecordSet = async (services, hostedZoneId) => {
   }
   const response = await services.route53.listResourceRecordSets(params).promise()
   const dnsRecordSet = response.ResourceRecordSets.find(r => r.Type === 'A')
-  console.log(`dnsRecordSet: ${JSON.stringify(dnsRecordSet, null, 2)}`)
+  console.log(`[findDnsRecordSet] dnsRecordSet: ${show(dnsRecordSet, toJson)}`)
   return dnsRecordSet
 }
 
-const createDnsRecordSet = async (services, hostedZoneId, newPublicIp) => {
-
+const changeDnsRecordSet = async (services, hostedZoneId, dnsRecordSet, action) => {
   const changeParams = {
     HostedZoneId: hostedZoneId,
     ChangeBatch: {
       Changes: [
         {
-          Action: 'CREATE',
-          ResourceRecordSet: {
-            "Name": "simple-app-jt.com.",
-            "Type": "A",
-            "TTL": 60,
-            "ResourceRecords": [
-              {
-                "Value": newPublicIp
-              }
-            ]
-          }
-        }
-      ]
-    }
-  }
-  const changeResponse = await services.route53.changeResourceRecordSets(changeParams).promise()
-  console.dir(changeResponse)
-
-  const waitParams = {
-    Id: changeResponse.ChangeInfo.Id
-  }
-  const waitResponse = await services.route53.waitFor('resourceRecordSetsChanged', waitParams).promise()
-  console.dir(waitResponse)
-}
-
-const updateDnsRecordSet = async (services, hostedZoneId, newPublicIp, dnsRecordSet) => {
-
-  const dnsRecordSetClone = R.clone(dnsRecordSet)
-  dnsRecordSetClone.ResourceRecords[0].Value = newPublicIp
-  console.log(`resourceRecordSetClone: ${JSON.stringify(dnsRecordSetClone, null, 2)}`)
-
-  const changeParams = {
-    HostedZoneId: hostedZoneId,
-    ChangeBatch: {
-      Changes: [
-        {
-          Action: 'UPSERT',
-          ResourceRecordSet: dnsRecordSetClone
-        }
-      ]
-    }
-  }
-  const changeResponse = await services.route53.changeResourceRecordSets(changeParams).promise()
-  console.dir(changeResponse)
-
-  const waitParams = {
-    Id: changeResponse.ChangeInfo.Id
-  }
-  const waitResponse = await services.route53.waitFor('resourceRecordSetsChanged', waitParams).promise()
-  console.dir(waitResponse)
-}
-
-const deleteDnsRecordSet = async (services, hostedZoneId, dnsRecordSet) => {
-
-  const changeParams = {
-    HostedZoneId: hostedZoneId,
-    ChangeBatch: {
-      Changes: [
-        {
-          Action: 'DELETE',
+          Action: action,
           ResourceRecordSet: dnsRecordSet
         }
       ]
     }
   }
+  console.log(`[changeDnsRecordSet] changeParams: ${show(changeParams, toJson)}`)
   const changeResponse = await services.route53.changeResourceRecordSets(changeParams).promise()
-  console.dir(changeResponse)
-
+  console.log(`[changeDnsRecordSet] changeResponse: ${show(changeResponse, toJson)}`)
   const waitParams = {
     Id: changeResponse.ChangeInfo.Id
   }
   const waitResponse = await services.route53.waitFor('resourceRecordSetsChanged', waitParams).promise()
-  console.dir(waitResponse)
+  console.log(`[changeDnsRecordSet] waitResponse: ${show(waitResponse, toJson)}`)
 }
 
-const doCreateOrUpdate = async services => {
+const deleteDnsRecordSet = async (services, hostedZoneId, dnsRecordSet) => {
+  await changeDnsRecordSet(services, hostedZoneId, dnsRecordSet, 'DELETE')
+}
+
+const upsertDnsRecordSet = async (services, hostedZoneId, newPublicIp) => {
+  const dnsRecordSet = {
+    Name: DOMAIN_NAME,
+    Type: 'A',
+    TTL,
+    ResourceRecords: [
+      {
+        Value: newPublicIp
+      }
+    ]
+  }
+  await changeDnsRecordSet(services, hostedZoneId, dnsRecordSet, 'UPSERT')
+}
+
+const doDelete = async (services, hostedZoneId, dnsRecordSet) => {
+  if (!dnsRecordSet) {
+    console.log('[doDelete] No DNS A record to delete - exiting!')
+    return
+  }
+  await deleteDnsRecordSet(services, hostedZoneId, dnsRecordSet)
+}
+
+const doUpsert = async (services, hostedZoneId, dnsRecordSet) => {
   const taskArn = await findTask(services)
+  if (!taskArn) {
+    console.log('[doUpsert] Failed to find task definition - exiting!')
+    return
+  }
   const eni = await findEni(services, taskArn)
   const newPublicIp = await findNewPublicIp(services, eni)
-  const hostedZoneId = await findHostedZoneId(services, 'simple-app-jt.com')
-  const dnsRecordSet = await findDnsRecordSet(services, hostedZoneId)
-  if (dnsRecordSet) {
-    if (dnsRecordSet.AliasTarget) {
-      console.log('DNS A record is an alias record - not updating!')
-      return
-    }
-    const oldPublicIp = dnsRecordSet.ResourceRecords[0].Value
-    console.log(`oldPublicIp: ${oldPublicIp}`)
-    if (newPublicIp !== oldPublicIp) {
-      await updateDnsRecordSet(services, hostedZoneId, newPublicIp, dnsRecordSet)
-    }
-  } else {
-    await createDnsRecordSet(services, hostedZoneId, newPublicIp)
-  }
-}
-
-const doDelete = async services => {
-  const hostedZoneId = await findHostedZoneId(services, 'simple-app-jt.com')
-  const dnsRecordSet = await findDnsRecordSet(services, hostedZoneId)
-  if (dnsRecordSet) {
-    if (dnsRecordSet.AliasTarget) {
-      console.log('DNS A record is an alias record - not deleting!')
-      return
-    }
-    await deleteDnsRecordSet(services, hostedZoneId, dnsRecordSet)
+  const oldPublicIp = dnsRecordSet ? dnsRecordSet.ResourceRecords[0].Value : undefined
+  console.log(`[doUpsert] oldPublicIp: ${show(oldPublicIp)}`)
+  if (!oldPublicIp || newPublicIp !== oldPublicIp) {
+    await upsertDnsRecordSet(services, hostedZoneId, newPublicIp)
   }
 }
 
@@ -178,11 +132,22 @@ const main = async () => {
       ec2: new AWS.EC2({ apiVersion: '2016-11-15', region: REGION }),
       route53: new AWS.Route53({ apiVersion: '2013-04-01', region: REGION })
     }
-    const lastArg = process.argv.slice(-1)[0]
+
+    const hostedZoneId = await findHostedZoneId(services, DOMAIN_NAME)
+    const dnsRecordSet = await findDnsRecordSet(services, hostedZoneId)
+
+    if (dnsRecordSet) {
+      if (dnsRecordSet.AliasTarget) {
+        console.log('[main] Existing DNS A record is an alias record - exiting!')
+        return
+      }
+    }
+
+    const lastArg = R.last(process.argv)
     if (['-d', '--delete'].includes(lastArg))
-      await doDelete(services)
+      await doDelete(services, hostedZoneId, dnsRecordSet)
     else
-      await doCreateOrUpdate(services)
+      await doUpsert(services, hostedZoneId, dnsRecordSet)
   } catch (error) {
     console.log(`[main] error: ${error.message}`)
   }
